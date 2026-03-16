74c56aff7bfb7f075a8e242fb200d6a91a3612d2
mumbaiops45 <mumbaiops45@gmail.com>
2026-03-16 17:37:30 +0530

    Add responsive category navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
 
         {/* Logo */}
-        <Link to="/">
+        <Link href="/">
           <div className="text-white font-bold text-xl tracking-wide">
             SALIL JAVERI
           </div>
